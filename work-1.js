066e7c0757c00b0f279318a565d3eda582d9ef01
Hovo Mnatsakanyan <51056620+Hovo91@users.noreply.github.com>
2019-08-06 20:55:56 +0400

    Create work-1.js

---
+++ work-1.js
@@ -0,0 +1,15 @@
+function checkArray(array = []) {
+  let checkArray = array.filter((item) => Array.isArray(item));
+  if (checkArray.length !== array.length) {
+    return 'Invalid Argument';
+    };
+  if (checkArray.every((item) => item.every((item) => item >= 0))) {
+    return 'No negatives';
+    };
+    let arrayWithNegs = checkArray.map((item) => item.filter((item) => item < 0));
+    // console.log(arrayWithNegs);
+    let arrayWithMaxNeg = arrayWithNegs.map((item) => item.length === 0 ? item.push(1) : item.reduce((min, item) => min > item ? min : item));
+  return arrayWithMaxNeg.reduce((product, elem) => product * elem);
+}
+
+console.log(checkArray([[2, -9, -3, 0], [1, 2], [-4 , -11, 0]]));
