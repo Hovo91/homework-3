69c614a3a2112d94e355d691e158423c0c6e6f63
Hovo Mnatsakanyan <51056620+Hovo91@users.noreply.github.com>
2019-08-06 15:02:59 +0400

    Update work-2.js

--- work-2.js
+++ work-2.js
@@ -9,3 +9,13 @@ function numberAndStringCounts(array = []) {
 }
 
 console.log(numberAndStringCounts([1, "‘10’", "‘hi’", 2, 3, 4, 2.3, 54, 5.2, -5, 0, '+']));
+
+// arrow
+
+function numberAndStringCounts(array = []) {
+  let arrayOfNum = array.filter((item) => (Math.floor(item) === item));
+  let arrayOfStr = array.filter((item) => (typeof item === 'string'));
+  return `Numbers: ${arrayOfNum.length}, Strings: ${arrayOfStr.length}`;
+}
+
+console.log(numberAndStringCounts([1, "‘10’", "‘hi’", 2, 3, 4, 2.3, 54, 5.2, -5, 0, '+']));
